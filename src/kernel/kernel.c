1efeab3f87b7ab1f5668904d9c7acbc6f9a60134
ThePurpleOne <jonas@stirnemann.xyz>
2022-02-02 00:26:30 +0100

    Working print function

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,14 +1,20 @@
+void print(char *text, int length);
+
 extern int main()
 {
 	char text[] = "Test";
+	print(text, 4);
+	return 0;
+}
 
+void print(char *text, int length)
+{
 	volatile char *buff = (volatile char *)0xb8000;
 	int i = 0;
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < length; i++)
 	{
 		*buff++ = text[i];
 		*buff++ = 0x02;
 	}
-
-	return 0;
+	return;
 }
