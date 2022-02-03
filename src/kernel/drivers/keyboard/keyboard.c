6a5374fe3f1bfc740e5adcb73e093f6c39b91dad
suguuss <spam.sugusgaming@gmail.com>
2022-02-03 22:01:20 +0100

    Added some keyboard stuff

---
+++ src/kernel/drivers/keyboard/keyboard.c
@@ -0,0 +1,43 @@
+/**
+ * @file keyboard.c
+ * @brief KEYBOARD OPERATIONS
+ * @version 0.1
+ * @date 03-02-2022
+ */
+
+#include "keyboard.h"
+#include "../print/print.h"		// TO REMOVE - DEBUG ONLY 
+
+// --------- PRIVATE DEFINES ---------
+// REGISTERS
+#define DATA_PORT 			0x60
+#define STATUS_REGISTER 	0x64	// READ
+#define COMMAND_REGISTER 	0x64	// WRITE
+
+// BITS
+#define BUFFER_STATUS_OUT	0x01
+#define BUFFER_STATUS_IN	0x02
+
+// -------- PRIVATE PROTOTYPE --------
+static void wait_for_output();
+static void wait_for_input();
+
+
+
+
+/**
+ * @brief Waits before reading an output from the
+ * keyboard controler
+ */
+static void wait_for_output()
+{
+	while (port_byte_in(STATUS_REGISTER) & BUFFER_STATUS_OUT){print("c");}
+}
+
+/**
+ * @brief Waits before writing to the keyboard controler
+ */
+static void wait_for_input()
+{
+	while (port_byte_in(STATUS_REGISTER) & BUFFER_STATUS_IN);
+}
---
+++ src/kernel/drivers/keyboard/keyboard.h
@@ -0,0 +1,15 @@
+/**
+ * @file keyboard.h
+ * @brief KEYBOARD OPERATIONS
+ * @version 0.1
+ * @date 03-02-2022
+ */
+
+#ifndef __KEYBOARD_H__
+#define __KEYBOARD_H__
+
+#include "../ports/ports.h"
+
+uint8_t test_controller();
+
+#endif
