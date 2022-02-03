d4b0d977b884bfcabfb7361730e9cd03faae2efd
ThePurpleOne <jonas@stirnemann.xyz>
2022-02-03 21:53:37 +0100

    Fixed the cprint + Added the \n parsing

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,14 +3,17 @@
 #include "drivers/print/print.h"
 
 #include <stdint.h>
-
 extern int main()
 {
-	char* msg1 = "Oh GRAHAM, receive my soul!";
+	char* msg1
+	  = "0000000\n00000000000000000000000000000000000000000000000000000000000000000000000000"
+		"000000000\n\n\n00000000\n\n\n\n\n00";
 	char* msg2 = "Thanks GRAHAM, You're the best!";
 
+	volatile uint16_t* buff = (volatile uint16_t*)VIDEO_ADDR;
+
+	// set_cursor(300);
 	print(msg1);
-	cprint(msg2, F_RED, F_BLACK);
 
 	return 0;
 }
