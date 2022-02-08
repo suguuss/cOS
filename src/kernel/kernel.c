32acb18d90f1b8dba9944976b34b6da117d7faf9
suguuss <spam.sugusgaming@gmail.com>
2022-02-08 13:51:51 +0100

    Forgot to add this file

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,22 +1,17 @@
 #include "drivers/disk/ata/ata.h"
 #include "drivers/screen/color/color.h"
 #include "drivers/screen/print/print.h"
+#include "stdlibs/string.h"
 
 #include <stdint.h>
 extern int main()
 {
-	char* msg1 = "GRAHAM THE BG\n\n";
-	char* msg2 = "Thanks GRAHAM, You're the best!";
+	char* msg1 = "This is a test string, fuck graham";
 
-	volatile uint16_t* buff = (volatile uint16_t*)VIDEO_ADDR;
+	memset(msg1, 'F', 4);
 
-	k_print_at(msg2, 1, 1);
-	set_foreground_color(FB_GREEN);
-
-	k_print_at(msg2, 1, 2);
-	set_background_color(FB_WHITE);
-
-	k_print_at(msg2, 1, 3);
+	set_font_color(FB_GREEN, FB_BLACK);
+	k_print(msg1);
 
 	return 0;
 }
