2232d3d075c3cb624c3ca506886d5ddc1c58935d
suguuss <spam.sugusgaming@gmail.com>
2022-02-08 13:50:23 +0100

    Added the memset function

---
+++ src/kernel/stdlibs/string.h
@@ -0,0 +1,15 @@
+/**
+ * @file string.h
+ * @brief STRING LIB
+ * @version 0.1
+ * @date 08-02-2022
+ */
+
+#ifndef __STRING_H__
+#define __STRING_H__
+
+#include <stdint.h>
+
+void memset(void *addr, uint8_t v, uint32_t n);
+
+#endif
