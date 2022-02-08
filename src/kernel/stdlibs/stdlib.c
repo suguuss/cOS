5ecf25b8f80a7ac7310e26a3b231745317b99433
ThePurpleOne <jonas@stirnemann.xyz>
2022-02-08 15:59:16 +0100

    Added the signed version of the itoa

--- src/kernel/stdlibs/stdlib.c
+++ src/kernel/stdlibs/stdlib.c
@@ -11,7 +11,7 @@
 
 /**
  * @breif Put a uint number into a string
- * STRING NEED TO BE LONG ENOUGH TO PUT THE NUMBER
+ * STRING NEEDS TO BE LONG ENOUGH TO PUT THE NUMBER
  * @param number
  * @param out_str
  */
@@ -26,5 +26,26 @@ void uitoa(uint16_t number, char* out_str)
 	{
 		uitoa(number / 10, out_str);
 		uitoa(number % 10, out_str + strlen(out_str));
+	}
+}
+
+/**
+ * @brief Put a int number into a string
+ * 	STRING NEEDS TO BE LONG ENOUGH TO PUT THE NUMBER
+ * @param number
+ * @param out_str
+ */
+void itoa(int16_t number, char* out_str)
+{
+	// If the number is nagative
+	if (number < 0)
+	{
+		// Put a minus sign and convert the inverse of the number
+		out_str[0] = '-';
+		uitoa(-number, out_str + 1);
+	}
+	else
+	{
+		uitoa(number, out_str);
 	}
 }
